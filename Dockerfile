ffbb237e8ba9441b9ce3541252ede2fb9d6fb932
Sandeep Kongathi <kn.sandeep@gmail.com>
2022-09-18 21:59:34 +0530

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,18 @@
 # Start by building the application.
-FROM golang:1.19 as build
+FROM golang:1.18 as build
 
 WORKDIR /go/src/app
 COPY . .
 
 RUN go mod download
-RUN CGO_ENABLED=0 GOOS=linux go build -a -o /go/bin/app .
+RUN CGO_ENABLED=0 go build -o /go/bin/app
 
 # Now copy it into our base image.
 FROM gcr.io/distroless/static-debian11
 COPY --from=build /go/bin/app /
-ENTRYPOINT [ "/app" ]
+CMD ["/app"]
+
 
 
 # docker build -t kafka-client-go .
-# docker run -it kafka-client-go 
+# docker run --network=host -it kafka-client-go 
--- main.go
+++ main.go
@@ -12,22 +12,26 @@ func main() {
 	// make a new reader that consumes from topic-A
 	r := kafka.NewReader(kafka.ReaderConfig{
 		//Brokers:  []string{"localhost:9092", "localhost:9093", "localhost:9094"},
-		Brokers:  []string{"localhost:53274"},
+		Brokers:  []string{"localhost:49838"},
 		GroupID:  "consumer-group-id",
 		Topic:    "test",
 		MinBytes: 10e3, // 10KB
 		MaxBytes: 10e6, // 10MB
 	})
 
+	fmt.Printf("Starting Consumer... \n")
+
 	for {
 		m, err := r.ReadMessage(context.Background())
 		if err != nil {
+			fmt.Printf("Error inside loop %s", err)
 			break
 		}
 		fmt.Printf("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
 	}
 
 	if err := r.Close(); err != nil {
+		fmt.Printf("Error outside loop %s", err)
 		log.Fatal("failed to close reader:", err)
 	}
 }
